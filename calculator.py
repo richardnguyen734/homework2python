bad35abdde82ac7750a69570322102e11d33aa69
richardnguyen734 <90486560+richardnguyen734@users.noreply.github.com>
2021-10-14 19:45:18 -0700

    Number 2 on python homework

---
+++ calculator.py
@@ -0,0 +1,30 @@
+import re
+
+def calculator(number1, number2, operator):
+    number1=float(number1)
+    number2=float(number2)
+    if(operator=="+"):
+        return(float(number1+number2))
+    if(operator=="-"):
+        return(float(number1-number2))
+    if(operator=="*"):
+        return(float(number1*number2))
+    if(operator=="/"):
+        return(float(number1/number2))
+    if(operator--"//"):
+        return(float(number1//number2))
+    if(operator=="**"):
+        return(float(number1**number2))
+    else:
+        return False
+
+def parse_input():
+    numbers1=""
+    numbers2=""
+    number1finished="False"
+    MathProblem=input("Enter equation: ")
+    answer=re.findall(r'[0-9\.]+',MathProblem)
+    print(calculator(answer[0], answer[2], answer[1]))
+
+               
+    
