93842097a12adfec0ab2a33a30f27ada67651b39
richardnguyen734 <90486560+richardnguyen734@users.noreply.github.com>
2021-10-14 20:12:48 -0700

    Update tripler.py

--- tripler.py
+++ tripler.py
@@ -4,5 +4,6 @@ def tripler(func):
         output=func(*args, **kwargs)
         output=func(*args, **kwargs)
         output=func(*args, **kwargs)
+        # Puts the output 3 times rather than 1 normally
         return output
     return wrapper
