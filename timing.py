f7d17faac58d1abac20cfc4c8414f0a601ed2cc0
richardnguyen734 <90486560+richardnguyen734@users.noreply.github.com>
2021-10-14 20:12:23 -0700

    Update timing.py

--- timing.py
+++ timing.py
@@ -3,6 +3,7 @@ import time
 def calculate_time(func):
     def wrapper():
         task=time.time()
+        # Inputs a time in seconds for the task involved
         func()
         print("Total time ", time.time()-task)
         
